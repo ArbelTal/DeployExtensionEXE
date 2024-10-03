6f04c8aef78998e136669154127010ccf7718a5b
ArbelTal <rbell.t@gmail.com>
2024-10-03 23:34:34 +0300

    run_command update

--- run_command.py
+++ run_command.py
@@ -1,13 +1,37 @@
 import subprocess
+from datetime import datetime
+import sys
+import os
 
+def get_resource_path(relative_path):
+    """ Get the path to the bundled resource, works when running as an executable """
+    try:
+        # If the script is running as an executable (bundled with PyInstaller)
+        base_path = sys._MEIPASS
+    except AttributeError:
+        # If running as a script (not bundled)
+        base_path = os.path.abspath(".")
+
+    return os.path.join(base_path, relative_path)
 
 def run_terminal_command():
     """
     Executes the terminal command to create an EXE using PyInstaller.
     """
     try:
-        # Define the pyinstaller command
-        command = ['pyinstaller', '--onefile', '--name', 'pyTal', 'DeployExtensionGitHub.py']
+        # Get the path to DeployExtensionGitHub.py, using resource path helper
+        script_path = get_resource_path('DeployExtensionGitHub.py')
+
+        # Get the current date in "yyyyMMdd" format
+        date_str = datetime.now().strftime("%Y%m%d")
+
+        # Define the command to run PyInstaller with the date-stamped name
+        command = [
+            "pyinstaller",
+            "--onefile",
+            "--name", f"pyTal_{date_str}",  # This names the executable with a date
+            script_path
+        ]
 
         # Run the terminal command
         subprocess.run(command, check=True)
